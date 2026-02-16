9b76f45d79ac49a8476816c4bb24b59b07882cd4
zhanghongbo415-sketch <zhanghongbo415@gmail.com>
2026-02-15 22:04:34 -0500

    Update msba-skill-sprint-Hongbo Zhang.R

--- msba-skill-sprint-Hongbo Zhang.R
+++ msba-skill-sprint-Hongbo Zhang.R
@@ -1,65 +1,107 @@
-# Q3
-# search atomic
+#' Pull out all cases that used to be contacts
+#'
+#' @param cases_clean The cleaned case data. Case data is returned by
+#' [`get_cases()`] and cleaned by [`clean_cases()`].
+#'
+#' @return A tibble containing the cases that used to be contacts.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' url <- "https://MyGoDataServer.com/"
+#' username <- "myemail@email.com"
+#' password <- "mypassword"
+#' outbreak_id <- "3b5554d7-2c19-41d0-b9af-475ad25a382b"
+#'
+#' cases <- get_cases(
+#'   url = url,
+#'   username = username,
+#'   password = password,
+#'   outbreak_id = outbreak_id
+#' )
+#'
+#' # other cleaned data required for `clean_cases()`
+#' cases_vacc_history_clean <- clean_case_vax_history(cases = cases)
+#' cases_address_history_clean <- clean_case_address_history(cases = cases)
+#' cases_dateranges_history_clean <- clean_case_med_history(cases = cases)
+#'
+#' cases_clean <- clean_cases(
+#'   cases = cases,
+#'   cases_address_history_clean = cases_address_history_clean,
+#'   cases_vacc_history_clean = cases_vacc_history_clean,
+#'   cases_dateranges_history_clean = cases_dateranges_history_clean
+#' )
+#'
+#' cases_from_contacts <- cases_from_contacts(cases_clean = cases_clean)
+#' }
+cases_from_contacts <- function(cases_clean) {
 
+  contacts_becoming_cases <- dplyr::filter(
+    .data = cases_clean,
+    .data$was_contact == TRUE
+  )
 
-# Q4
-x<-4
-class(x)
+  # set this status to became case and no longer active
+  contacts_becoming_cases <- dplyr::mutate(
+    .data = contacts_becoming_cases,
+    follow_up_status = "BECAME_CASE",
+    was_case = NA,
+    date_of_last_contact = NA,
+    follow_up_team_id = NA,
+    relationship_exposure_type = NA,
+    relationship_context_of_transmission = NA,
+    relationship_exposure_duration = NA,
+    relationship_exposure_frequency = NA,
+    relationship_certainty_level = NA,
+    relationship_cluster_id = NA,
+  )
 
-# Q5
-y <- c(4, TRUE)
-y
-class(y)
+  # organize order of vars, only bring in what we need, take away confusing vars
+  contacts_becoming_cases <- dplyr::select(
+    .data = contacts_becoming_cases,
+    "id", # identifier
+    "visual_id", # identifier
+    "classification", # identifier
+    "follow_up_status", # identifier
+    "first_name", # demographics
+    "middle_name", # demographics
+    "last_name", # demographics
+    "gender", # demographics
+    "age", # demographics
+    "age_class", # demographics
+    "occupation", # demographics
+    "pregnancy_status", # demographics
+    "date_of_reporting", # dates
+    "date_of_last_contact", # dates
+    "date_of_burial", # dates
+    "risk_level", # epi
+    "risk_reason", # epi
+    "responsible_user_id", # assigned contact tracer
+    "follow_up_team_id", # assigned contact tracer
+    dplyr::matches("^admin_.*name$"), # address
+    "lat", # address
+    "long", # address
+    "address", # address
+    "postal_code", # address
+    "city", # address
+    "telephone", # address
+    "email", # address
+    "vaccinated",
+    "outcome", # outcome
+    "date_of_outcome", # outcome
+    "relationship_exposure_type",
+    "relationship_context_of_transmission",
+    "relationship_exposure_duration",
+    "relationship_exposure_frequency",
+    "relationship_certainty_level",
+    "relationship_cluster_id",
+    "location_id", # uuid in case need later for joining of whatever sort
+    "created_by", # record modification
+    "datetime_created_at", # record modification
+    "updated_by", # record modification
+    "datetime_updated_at" # record modification
+  )
 
-# Q6
-a <- c(3, 2, 10)
-b <- c(1,3, 5) 
-cbind(a,b)
-
-#Q7
-dim(a)
-
-# Q8
-c <- list(2, "a", "b", TRUE)
-c[[1]]
-
-# Q9
-d <- 1:4
-e <- 2:3
-class(d+e)
-
-
-# Q 10
-f <- c(17, 14, 4, 5, 13, 12, 10) 
-f[f > 10] <- 4
-
-# Q11, 12, 13, 14
-names(hw1_data)
-nrow(hw1_data)
-hw1_data[1:2,]
-tail(hw1_data)
-
-## Q 15-20
-
-# What is the value of Ozone in the 47th row?
-hw1_data[47,]
-
-# How many missing values are in the Ozone column of this data frame
-sum(is.na(hw1_data$Ozone))
-# What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
-mean(na.omit(hw1_data$Ozone))
-
-# Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. 
-# What is the mean of Solar.R in this subset?
-hw1_data.sub1 <- subset(hw1_data, Ozone > 31 & Temp > 90)
-mean(hw1_data.sub1$Solar.R)
-
-# What is the mean of "Temp" when "Month" is equal to 6? 
-hw1_data.sub2 <- subset(na.omit(hw1_data), Month == 6)
-mean(hw1_data.sub2$Temp)
-
-# What was the maximum ozone value in the month of May (i.e. Month = 5)?
-hw1_data.sub3 <- subset(na.omit(hw1_data), Month == 5)
-max(hw1_data.sub3$Ozone)
-
+  return(contacts_becoming_cases)
+}
 
